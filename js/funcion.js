cad55aba30c5854a912a4273e4ab4156c16f04c4
JesusRed <jalfredo1954@gmail.com>
2019-10-07 11:04:12 -0500

    validado login

--- js/funcion.js
+++ js/funcion.js
@@ -7,5 +7,15 @@ $(document).ready(function () {
 $(function(){
     $("#login_btn").click(function(){
         $("#login_btn").hide();
+
+        var username = $("#name").val();
+        var password = $("#pass").val();
+
+        if(username=="admin" && password=='1234'){
+            window.location = "datos.html"
+        }else{
+            $("#login_message").text("Datos Inválidos");
+        }
+            
     });
 });
